d8c817629158daf9c623129e635b1079a4ee5196
Karyna Zuyeva <kzueva54@gmail.com>
2024-11-22 13:59:23 +0000

    modified:   login_2fa.py 	modified:   login_2fa.py

--- login_2fa.py
+++ login_2fa.py
@@ -1,6 +1,47 @@
-import os
 import time
+from playwright.sync_api import sync_playwright
 
-from playwright.sync_api import sync_playwright 
-from bs4 import BeautifulSoup 
-import pandas as pd
+def open_login_page(page):
+    """
+    Opens the login page.
+    """
+    page.goto("https://eq.hsc.gov.ua/")
+
+def select_checkbox(page):
+    """
+    Selects the checkbox near the text "Я ознайомлений та погоджуюсь з умовами надання послуги".
+    """
+    page.wait_for_selector('input[type="checkbox"]')
+    checkbox = page.locator('input[type="checkbox"]')
+    checkbox.click()
+
+def click_sign_up_button(page):
+    """
+    Clicks the "е-запис" button after the checkbox is selected.
+    """
+    page.wait_for_selector('a.btn.btn-lg.btn-hsc-green_s')
+    button = page.locator('a.btn.btn-lg.btn-hsc-green_s')
+    button.click()
+
+def main():
+    """
+    Main function to automate the process.
+    """
+    with sync_playwright() as p:
+        # Launch the browser (set headless=False to see UI)
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+
+        open_login_page(page)
+        # Select the checkbox
+
+        select_checkbox(page)
+        time.sleep(10)
+
+        # Click the "е-запис" button
+        click_sign_up_button(page)
+        time.sleep(10)
+        browser.close()
+
+if __name__ == "__main__":
+    main()
