3f45f5f364362babf1d96fd1897d904903bc7784
Karyna <kzueva54@gmail.com>
2024-11-24 03:29:37 +0200

    modified:   tests/__pycache__/test_constants.cpython-311.pyc 	modified:   tests/test_constants.py

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -1,2 +1,2 @@
-JKS_FILE_PATH_MAC = "/Users/caroline/Desktop/ticket-catcher/user_files/test.jks"
+JKS_FILE_PATH_MAC = "/Users/caroline/Desktop/ticket-catcher/user_files/mine.jks"
 PASSWORD_FILE_PATH_MAC = "/Users/caroline/Desktop/ticket-catcher/user_files/password_for_jks.txt"
